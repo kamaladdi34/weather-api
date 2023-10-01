53e52bd5645fbc689195711d5aea0dc779e197f4
Addi Kamal <kamal.addi@proton.me>
2023-10-01 15:17:34 +0100

    Test API request

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 import '../style.css';
+const API_KEY = 'fbaf5237221c4603adf154400232809';
+const getWeatherData = async (location) => {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=yes`,
+    { mode: 'cors' },
+  );
+  console.log(await response.json());
+};
+getWeatherData('agadir');
