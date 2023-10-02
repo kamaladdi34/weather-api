29bad7e10f8c882a09389b4565e2edb9c1b2b6f5
Addi Kamal <kamal.addi@proton.me>
2023-10-02 12:25:41 +0100

    Add temperature scale switch functionality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import './style.css';
 const temperature = document.querySelector('.info-card .card-stats .amount');
+const temperatureScale = document.querySelector(
+  '.info-card .card-stats .scale',
+);
 const locationName = document.querySelector('.location .name');
 const locationRegion = document.querySelector('.location .region');
 const locationCountry = document.querySelector('.location .country');
@@ -17,6 +20,8 @@ const airQualityNumber = document.querySelector(
 );
 const airQualityInfo = document.querySelector('.air-quality .card-info');
 const API_KEY = 'fbaf5237221c4603adf154400232809';
+const temperatureToggle = document.querySelector('#temperature-toggle');
+let temperatureC = 0;
 const requestWeatherData = async (location) => {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=yes`,
@@ -85,8 +90,8 @@ const getAirQualityInfo = (value) => {
 };
 const displayWeatherData = (location) => {
   requestWeatherData(location).then((response) => {
-    console.log(response);
-    temperature.innerText = response.current.temp_c;
+    setTemperature(response.current.temp_c, !temperatureToggle.checked);
+    temperatureC = response.current.temp_c;
     locationName.innerText = response.location.name;
     locationRegion.innerText = response.location.region;
     locationCountry.innerText = response.location.country;
@@ -96,7 +101,6 @@ const displayWeatherData = (location) => {
     humidityInfo.innerText = getHumidityInfo(response.current.humidity);
     windSpeedNumber.innerText = response.current.wind_kph;
     windSpeedInfo.innerText = getWindSpeedInfo(response.current.wind_kph);
-    console.log(response.current.air_quality);
     airQualityNumber.innerText = response.current.air_quality['gb-defra-index'];
     airQualityInfo.innerText = getAirQualityInfo(
       response.current.air_quality['gb-defra-index'],
@@ -112,4 +116,16 @@ document.addEventListener('keydown', (event) => {
   ) {
     displayWeatherData(searchInput.value);
   }
+});
+const setTemperatureValues = (value, scale) => {
+  temperature.innerText = value;
+  temperatureScale.innerText = scale;
+};
+function setTemperature(temperature, isCelsius) {
+  const temp = isCelsius ? temperature : (temperature * 9) / 5 + 32;
+  const symbol = isCelsius ? '°C' : '°F';
+  setTemperatureValues(temp, symbol);
+}
+temperatureToggle.addEventListener('change', (event) => {
+  setTemperature(temperatureC, !event.target.checked);
 });
